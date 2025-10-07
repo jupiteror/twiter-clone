35275931e06335cb62c277e9275da9f5afa7f115
jupiteror <kirill123iop@gmail.com>
2025-10-07 15:50:07 +0500

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../components/ui/button";
+import AppRouter from "./AppRouter";
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
